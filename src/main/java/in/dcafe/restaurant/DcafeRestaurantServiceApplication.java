b3d59c83a6b9d528a705b37089b0913018b34745
Kalyan1718 <durgakalyan018@gmail.com>
2024-08-14 13:50:24 +0530

    First Commit

---
+++ src/main/java/in/dcafe/restaurant/DcafeRestaurantServiceApplication.java
@@ -0,0 +1,13 @@
+package in.dcafe.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcafeRestaurantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcafeRestaurantServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/dcafe/restaurant/DcafeRestaurantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.dcafe.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcafeRestaurantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
